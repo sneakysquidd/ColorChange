ae46a843de4d9c9bdac2db87f2eeaa7f56cdf7c2
_gavbob <gsct2002@gmail.com>
2018-10-09 12:21:04 -0600

    Change viewcontroller

---
+++ ColorChange/Model/ColorTool.swift
@@ -0,0 +1,16 @@
+//
+//  ColorTool.swift
+//  ColorChange
+//
+//  Created by Thomas, Gavin on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+public class ColorTool
+{
+    
+}
